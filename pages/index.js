5e99191709beb81baf010f3b8eb87213d659f430
Olivia <oliviabelu@web.de>
2026-06-12 08:41:57 +0200

    fetch data and error handling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 import Board from "@/components/Tile/Board";
 
+import { useState, useEffect } from "react";
+
 export default function HomePage() {
+  const [wordSet, setWordSet] = useState();
+
+  async function loadWords() {
+    try {
+      const response = await fetch("./words.json");
+      const wordArray = await response.json();
+
+      const set = new Set(wordArray);
+      setWordSet(set);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    loadWords();
+  }, []);
+
+  console.log(wordSet);
   return (
     <>
       <h1>Scrabboli</h1>
