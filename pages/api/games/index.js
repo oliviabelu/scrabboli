a3b4bed91aa9fbd3c9d68bdec2c467d846f874be
Olivia <oliviabelu@web.de>
2026-06-12 12:18:34 +0200

    added games GET method for api

---
+++ pages/api/games/index.js
@@ -0,0 +1,16 @@
+import dbConnect from "@/db/connect";
+import Game from "@/db/models/Game";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  try {
+    if (request.method === "GET") {
+      const games = await Game.find();
+      return response.status(200).json(games);
+    }
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ status: "Internal Server Error." });
+  }
+  return response.status(405).json({ status: "Method not allowed." });
+}
